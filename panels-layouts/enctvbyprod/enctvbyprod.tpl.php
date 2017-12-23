32ed8747a0e22fd897738a5405f8e51a56fb3b77
Ilya Kushlianski <kushliansky@gmail.com>
2017-12-23 15:21:43 +0300

    Add regions for navigation to main panel

--- panels-layouts/enctvbyprod/enctvbyprod.tpl.php
+++ panels-layouts/enctvbyprod/enctvbyprod.tpl.php
@@ -12,8 +12,76 @@
  *   $content['middle']: The only panel in the layout.
  */
 ?>
-<div class="panel-display panel-1col clearfix" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
-  <div class="panel-panel panel-col">
-    <div><?php print $content['middle']; ?></div>
+<div class="region-sitemap-nav">
+  <?php print $content['sitemap-nav'];?>
+</div>
+
+<div id="page-wrapper">
+
+  <div id="page">
+    <div id="header">
+      <div class="section clearfix">
+        <div id="navigation">
+          <div class="section nav-group-mobile">
+            <?php print $content['main-menu-mobile'];?>
+          </div>
+          <div class="section nav-group1-desktop">
+            <?php print $content['main-menu-1'];?>
+          </div>
+          <div class="section nav-group2-desktop">
+            <h4 class="hot-topics-heading">Trending</h4>
+            <?php print $content['main-menu-2'];?>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div id="main-wrapper">
+      <div id="main">
+
+        <div id="front-page-content-promoted" class="column">
+
+          <div class="section">
+            <div class="region region-fp-five-main-items">
+            <?php print $content['region-fp-five-main-items'];?>
+            </div>
+          </div>
+
+          <div class="section">
+            <div class="region region-fp-three-submain-sections">
+            <?php print $content['region-fp-three-submain-sections'];?>
+            </div>
+          </div>
+
+          <div class="section">
+            <div class="region region-fp-media">
+            <?php print $content['region-fp-media'];?>
+            </div>
+          </div>
+
+        </div>
+
+        <div id="fp-other-content-wrapper">
+          <div id="fp-other-content-1" class="clearfix">
+          <?php print $content['fp-other-content-1'];?>
+          </div>
+          <div id="fp-other-content-2" class="clearfix">
+          <?php print $content['fp-other-content-2'];?>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    <div id="footer">
+      <div class="section">
+        <footer class="footer" role="contentinfo">
+          <div class="footer-content">
+          <?php print $content['footer'];?>
+          </div>
+        </footer>
+      </div>
+    </div>
+
   </div>
 </div>
